f451f27731e3b2605371e8f8aaa2571b6a793719
Ivan Sidaruk <isidaruk@yahoo.com>
2018-09-17 20:57:06 +0300

    Added crawler.py

---
+++ crawler.py
@@ -0,0 +1,12 @@
+"""
+Crawler
+
+Script is used for downloading job page from URL https://www.snap.com/en-US/jobs/ and save the
+extracted page info into an HTML file on the disk:
+
+Input
+$ python crawler.py https://www.snap.com/en-US/jobs/ snap.html
+
+Output
+snap.html
+"""
